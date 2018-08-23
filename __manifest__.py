854974971b9f806fc1f519ef8ffca84c0b0d9d79
exzone97 <exzone97@yahoo.co.id>
2018-08-23 08:42:02 +0700

    Initial commit

---
+++ __manifest__.py
@@ -0,0 +1,13 @@
+{
+	'name':'Tutorial theme',
+	'description': 'A description for your theme.',
+	'version':'1.0',
+	'author':'Your name',
+	'data': [
+		'views/snippets.xml',
+		],
+	'category': 'Theme/Creative',
+	'depends': ['website','web'],
+	'installable': True,
+	'application': True,
+}
